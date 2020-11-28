bd671b0279bfc6bcd56306db077c3c15e719ff19
kizuna365 <kizuna365kizuna@gmail.com>
2020-11-28 16:40:03 +0900

    一個前に更新したものにバグがあったため修正

--- 1cc.c
+++ 1cc.c
@@ -171,9 +171,9 @@ Node *primary() {
 
 Node *unary() {
   if (consume('+'))
-    return primary();
+    return unary();
   if (consume('-'))
-    return new_node(ND_SUB, new_node_num(0), primary());
+    return new_node(ND_SUB, new_node_num(0), unary());
   return primary();
 }
 
